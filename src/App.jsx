f85f1434ef344378fdf6e84f48de4a7566c9d93a
Leslie <leslie.bagalho@yahoo.com.br>
2025-08-15 21:25:12 -0300

    Criando o radapé

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/page";
 import Sobre from "./pages/sobre/page";
+import Footer from "./components/Footer/Footer";
 
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
           <Route path="/sobre" element={<Sobre />} />
           {/* Outras rotas aqui */}
         </Routes>
+        <Footer />
       </Router>
     </>
   )
